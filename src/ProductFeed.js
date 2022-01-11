188e0e9eb1fb50c61221d499c6b7c1642c84e2b3
eslamzaaki <solomzaki840@gmail.com>
2022-01-11 17:05:59 +0200

    1

--- src/ProductFeed.js
+++ src/ProductFeed.js
@@ -2,7 +2,6 @@ import Product from "./Product";
 import React from "react";
 
 function ProductFeed({ products }) {
-	console.log(products);
 	return (
 		<div className="w-full p-5 grid  grid-flow-row-dense bg-gray-100 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
 			{products
